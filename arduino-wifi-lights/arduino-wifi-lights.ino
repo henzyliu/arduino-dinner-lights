1d75bbf572f00526adabbbf64a46374370f8cce9
Henry Liu <henzyliu@gmail.com>
2022-08-14 00:39:04 +1200

    added pin defines

--- arduino-wifi-lights/arduino-wifi-lights.ino
+++ arduino-wifi-lights/arduino-wifi-lights.ino
@@ -4,12 +4,18 @@
 /*Defines*/
 #define SERIAL_SPEED 115200
 
+#define LED_PIN 2
+
 /*Global Variables*/
 const char* ssid = "Liu Family";
 const char* password = "1a2s3h4l5e6y7";
 
+bool ledStatus = HIGH;
+
 void setup() {
+  Serial.begin(SERIAL_SPEED);
   
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
