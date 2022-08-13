7bde6171a20758e92d7bcd3303d8d959b6f936aa
Henry Liu <henzyliu@gmail.com>
2022-08-14 00:32:37 +1200

    added base arduino function

---
+++ arduino-wifi-lights/arduino-wifi-lights.ino
@@ -0,0 +1,17 @@
+/*Includes*/
+#include <WiFi.h>
+
+/*Defines*/
+#define SERIAL_SPEED 115200
+
+/*Global Variables*/
+const char* ssid = "Liu Family";
+const char* password = "1a2s3h4l5e6y7";
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
